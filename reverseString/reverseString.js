6c5f94b0efff5cfe3f5367ee77568ee3725c09b7
Javier Olayon <olayon.javier@gmail.com>
2020-10-13 23:16:44 -0400

    finsihed reverseString exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let returnString = "";
+    for(let i = input.length; i >= 0; i--){
+        returnString += input.charAt(i);
+    }
 
+    return returnString;
 }
 
 module.exports = reverseString
