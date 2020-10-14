3263384ebcb05ef3bbfd4aa6828b2ec762dc90c9
Javier Olayon <olayon.javier@gmail.com>
2020-10-13 23:39:07 -0400

    finished removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
 const removeFromArray = function() {
+    let inputArr = arguments[0];
+    for (let i = 1; i < arguments.length; i++){
+        inputArr = removalHelp(inputArr, arguments[i]);
+    }
+    return inputArr;
+}
 
+function removalHelp(inputArr, itemToRemove){
+    for (let i =0; i < inputArr.length; i++){
+        if(itemToRemove === inputArr[i]){
+            inputArr.splice(i, 1);
+        }
+    }
+    return inputArr;
 }
 
 module.exports = removeFromArray
