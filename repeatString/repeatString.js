1786a908e4347ec286810073521455519832f2fb
Javier Olayon <olayon.javier@gmail.com>
2020-10-13 23:12:53 -0400

    finsihed repeatString exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, numOfRepeats) {
+    let returnString = ""
+    if (numOfRepeats < 0){
+        return "ERROR";
+    }
+    for(let i = 0; i < numOfRepeats; i++){
+        returnString += inputString;
+    }
+    return returnString;
 }
 
 module.exports = repeatString
