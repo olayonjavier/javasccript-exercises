82b6bbabc6508f1a3b05c56dc982c0f9f221e72a
Javier Olayon <olayon.javier@gmail.com>
2020-10-13 23:46:41 -0400

    finished sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let sum = 0;
+    if(int1 < 0 || int2 < 0 || typeof (int1) != "number" || typeof (int2) != "number"){
+        sum = "ERROR";
+    }else if(int1 <= int2){
+        for(let i = int1; i <= int2; i++){
+            sum += i;
+        }
+    }else{
+        for(let i = int2; i <= int1; i++){
+            sum += i;
+        }
+    }
+    
+    return sum;
 }
 
 module.exports = sumAll
