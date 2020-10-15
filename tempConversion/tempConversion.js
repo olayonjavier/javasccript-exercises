8674ac361dea71c124acacbd8d2d57fe85f7a97a
Javier Olayon <olayon.javier@gmail.com>
2020-10-15 19:16:30 -0400

    finished tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const ftoc = function() {
+const ftoc = function(input) {
+  let result = ((input - 32) * 5/9);
 
+  if(result.toString().includes(".")){
+    return Math.round(result * 1e1) / 1e1;
+  }else {
+    return result;
+  }
 }
 
-const ctof = function() {
-
+const ctof = function(input) {
+  let result = (input * 9/5 + 32);
+  if(result.toString().includes(".")){
+    return Math.round(result * 1e1) / 1e1;
+  }else{
+    return result;
+  }
 }
 
 module.exports = {
